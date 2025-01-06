87a5e8e1a1f9c47f124df8169cc2e87097015578
Jneck <parklim2254@gmail.com>
2025-01-06 20:25:38 +0900

    fix: update post dto 수정

--- src/module/domain/posts/dtos/update-post.dto.ts
+++ src/module/domain/posts/dtos/update-post.dto.ts
@@ -4,6 +4,7 @@ import {
   IsArray,
   IsEnum,
   IsNumber,
+  IsOptional,
   IsString,
   ValidateNested,
 } from 'class-validator';
@@ -17,17 +18,18 @@ export class UpdatePostDto {
   @IsString()
   title: string;
 
-  @ApiPropertyOptional({
+  @ApiProperty({
     description: '발행 여부',
     enum: PostStatus,
   })
   @IsEnum(PostStatus)
   status: PostStatus;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: '글에 작성된 여행의 총 비용, 기준은 대한민국 원화',
   })
   @IsNumber()
+  @IsOptional()
   totalExpense: number;
 
   @ApiProperty({
