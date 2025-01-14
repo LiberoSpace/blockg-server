d4112e43c0867414d88b2368df11287a594c2f64
Jneck <parklim2254@gmail.com>
2025-01-15 00:39:34 +0900

    chore: 환율 엔드포인트 안보이게 변경

--- src/module/domain/exchange-rate/exchange-rate.controller.ts
+++ src/module/domain/exchange-rate/exchange-rate.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { ExchangeRateService } from './exchange-rate.service';
 import { CloudSchedulerGuard } from '../../../guards/cloud-scheduler.guard';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller('blockg/api/v1/exchange-rate')
 export class ExchangeRateController {
   constructor(private readonly exchangeRateService: ExchangeRateService) {}
 
+  @ApiExcludeEndpoint()
   @UseGuards(CloudSchedulerGuard)
   @Post('')
   async fetchExchangeRate() {
