b56398e8a2f10dcf214f1e8691e89bce120511af
Jneck <parklim2254@gmail.com>
2025-01-31 00:52:17 +0900

    feat: 공개용 API에서 개수세지 못하도록 조정

--- src/module/domain/post/controllers/post.public.controller.ts
+++ src/module/domain/post/controllers/post.public.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Query,
+} from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PostService } from '../services/post.service';
 import { GetPostsDto } from './dtos/get-posts.dto';
@@ -18,6 +26,11 @@ export class PostPublicController {
   })
   @Get('/')
   async getPosts(@Query() dto: GetPostsDto): Promise<GetPostsRdto> {
+    if (dto.page * dto.limit + dto.limit > 20) {
+      throw new BadRequestException(
+        '공개용 API에서는 20개 넘게 요청할 수 없습니다.',
+      );
+    }
     const postPage = await this.postService.findPage(dto);
     return GetPostsRdto.fromPage(postPage);
   }
